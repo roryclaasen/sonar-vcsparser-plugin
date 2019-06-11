ef746779864b9029a52b4543a181dbe504c4a88f
Rory Claasen <t-roclaa@microsoft.com>
2019-06-11 17:38:01 +0100

    Fixed 3 bugs from sonarcloud

--- src/main/java/dev/roryclaasen/vcsparser/system/FileReader.java
+++ src/main/java/dev/roryclaasen/vcsparser/system/FileReader.java
@@ -5,7 +5,6 @@ package dev.roryclaasen.vcsparser.system;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -18,14 +17,13 @@ public class FileReader implements IFileReader {
 
 	private final Logger log = Loggers.get(FileReader.class);
 
-	@SuppressWarnings("resource")
 	@Override
 	public String readFile(String fileName) {
-		try {
-			InputStream stream = new FileInputStream(fileName);
-			InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
-			return new BufferedReader(reader).lines().collect(Collectors.joining());
-		} catch (FileNotFoundException e) {
+		try (InputStream stream = new FileInputStream(fileName);
+				InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+				BufferedReader br = new BufferedReader(reader)) {
+			return br.lines().collect(Collectors.joining());
+		} catch (Exception e) {
 			log.error("Could not read file: " + fileName);
 			return null;
 		}
